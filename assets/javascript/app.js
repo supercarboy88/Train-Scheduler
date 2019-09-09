f48601e33fa17ec70fa173b2cd97171b3b269e0b
YI YING <supercarboy88@gmail.com>
2019-09-09 01:00:03 -0700

    add style to tabel and create app.js

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDclNjk-yy3r68O-NiSOaL_ST1PVDccx54",
+    authDomain: "uw-bootcamp2019.firebaseapp.com",
+    databaseURL: "https://uw-bootcamp2019.firebaseio.com",
+    projectId: "uw-bootcamp2019",
+    storageBucket: "uw-bootcamp2019.appspot.com",
+    messagingSenderId: "689225463206",
+    appId: "1:689225463206:web:5a5abed561acfc58"
+
+};
+firebase.initializeApp(config);
